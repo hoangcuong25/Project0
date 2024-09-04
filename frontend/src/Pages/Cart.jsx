faf355147474c557678a8f8463628138c557e446
hoang cuong <vancuong25112003@gmail.com>
2024-09-04 23:29:11 +0700

    push

---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Cart = () => {
+    <div>
+
+    </div>
+
+}
+
+export default Cart
---
+++ frontend/src/Pages/Product.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Product = () => {
+    <div>
+
+    </div>
+}
+
+export default Product
